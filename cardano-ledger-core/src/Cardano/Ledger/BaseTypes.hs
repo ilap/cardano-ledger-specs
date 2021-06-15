fc17a4fc2eb77f80b711991f2400dfb3e131fe47
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2021-06-15 17:50:34 +0300

    Initital implementation of BoundedRatio

--- cardano-ledger-core/src/Cardano/Ledger/BaseTypes.hs
+++ cardano-ledger-core/src/Cardano/Ledger/BaseTypes.hs
@@ -5,12 +5,16 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Cardano.Ledger.BaseTypes
@@ -64,12 +68,12 @@ import Cardano.Binary
 import Cardano.Crypto.Hash
 import Cardano.Crypto.Util (SignableRepresentation (..))
 import qualified Cardano.Crypto.VRF as VRF
-import Cardano.Ledger.Serialization (decodeRecordSum, ratioToCBOR, ratioFromCBOR)
+import Cardano.Ledger.Serialization (decodeRecordSum, ratioFromCBOR, ratioToCBOR)
 import Cardano.Prelude (NFData, cborError)
 import Cardano.Slotting.EpochInfo
 import Cardano.Slotting.Time (SystemStart)
 import Control.Exception (throw)
-import Control.Monad (when)
+import Control.Monad (when, (<=<))
 import Control.Monad.Trans.Reader (ReaderT)
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Data.Binary.Put as B
@@ -81,10 +85,11 @@ import qualified Data.Fixed as FP (Fixed, HasResolution, resolution)
 import Data.Functor.Identity
 import Data.Maybe.Strict
 import Data.Ratio (Ratio, denominator, numerator, (%))
-import Data.Scientific (Scientific, scientific, base10Exponent, coefficient, normalize)
+import Data.Scientific (Scientific, base10Exponent, coefficient, normalize, scientific)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Data.Text.Encoding (encodeUtf8)
+import Data.Typeable
 import Data.Word (Word16, Word64, Word8)
 import GHC.Exception.Type (Exception)
 import GHC.Generics (Generic)
@@ -104,24 +109,118 @@ type FixedPoint = Digits34
 fpPrecision :: FixedPoint
 fpPrecision = (10 :: FixedPoint) ^ (34 :: Integer)
 
-newtype RawUnit
-  = RawUnit Word64
-  deriving newtype (Show, Eq, Ord, Num, Real, Enum, Integral, NFData)
+-- | This is an internal type for representing rational numbers that are bounded on some
+-- interval that is controlled by phantom type variable @b@ as well as by
+-- the bounds of underlying type @a@.
+newtype BoundedRatio b a = BoundedRatio (Ratio a)
+  deriving (Ord, Eq, Generic)
+  deriving newtype (Show, NoThunks, NFData)
+
+promoteRatio :: Integral a => Ratio a -> Rational
+promoteRatio r = toInteger (numerator r) % toInteger (denominator r)
+
+boundedRatioToRational :: Integral a => BoundedRatio b a -> Rational
+boundedRatioToRational (BoundedRatio r) = promoteRatio r
+
+boundedRatioFromRational ::
+  forall b a.
+  (Bounded (BoundedRatio b a), Bounded a, Integral a) =>
+  Rational ->
+  Maybe (BoundedRatio b a)
+boundedRatioFromRational r
+  | n < minVal || d < minVal || n > maxVal || d > maxVal = Nothing -- protect against overflow
+  | otherwise = internalBoundedRatioFromRatio $ fromInteger n % fromInteger d
+  where
+    minVal = toInteger (minBound :: a)
+    maxVal = toInteger (maxBound :: a)
+    n = numerator r
+    d = denominator r
+
+internalBoundedRatioFromRatio ::
+  forall b a.
+  (Bounded (BoundedRatio b a), Integral a) =>
+  Ratio a ->
+  Maybe (BoundedRatio b a)
+internalBoundedRatioFromRatio ratio
+  | r < boundedRatioToRational lowerBound
+      || r > boundedRatioToRational upperBound =
+    Nothing -- ensure valid range
+  | otherwise = Just $ BoundedRatio ratio
+  where
+    r = promoteRatio ratio
+    lowerBound = minBound :: BoundedRatio b a
+    upperBound = maxBound :: BoundedRatio b a
+
+instance (ToCBOR a, Integral a, Bounded a, Typeable b, Typeable a) => ToCBOR (BoundedRatio b a) where
+  toCBOR (BoundedRatio u) = ratioToCBOR u
+
+instance
+  (FromCBOR a, Bounded (BoundedRatio b a), Bounded a, Integral a, Typeable b, Typeable a, Show a) =>
+  FromCBOR (BoundedRatio b a)
+  where
+  fromCBOR = do
+    r <- ratioFromCBOR
+    case internalBoundedRatioFromRatio r of
+      Nothing ->
+        cborError $ DecoderErrorCustom "UnitInterval" (Text.pack $ show r)
+      Just u -> pure u
+
+instance ToJSON (BoundedRatio b Word64) where
+  toJSON = toJSON . boundedRatio64ToScientificRounding
+
+boundedRatio64ToScientificRounding :: BoundedRatio b Word64 -> Scientific
+boundedRatio64ToScientificRounding (boundedRatioToRational -> r) =
+  scientific
+    ((numerator r * scale) `quot` denominator r)
+    exp10
+  where
+    scale = 10 ^ negate exp10
+    exp10 = -19
+
+instance Bounded (BoundedRatio b Word64) => FromJSON (BoundedRatio b Word64) where
+  parseJSON = either fail pure . boundedRatio64FromScientific <=< parseJSON
+
+boundedRatio64FromScientific ::
+  Bounded (BoundedRatio b Word64) =>
+  Scientific ->
+  Either String (BoundedRatio b Word64)
+boundedRatio64FromScientific (normalize -> sci) = do
+  -- protect against unbounded memory consumption:
+  when (abs exp10 > 20 || coeff > toInteger (maxBound :: Word64)) $
+    Left "Precision is too large"
+  maybe (Left "Value is outside of [0, 1] range") Right $
+    boundedRatioFromRational (coeff % 10 ^ negate exp10)
+  where
+    coeff = coefficient sci
+    exp10 = base10Exponent sci
+
+-- | Type to represent a value in the unit interval (0; 1]
+newtype PositiveUnitInterval = PositiveUnitInterval (BoundedRatio PositiveUnitInterval Word64)
+  deriving (Ord, Eq, Generic)
+  deriving newtype (Show, Bounded, ToCBOR, FromCBOR, ToJSON, FromJSON, NoThunks, NFData)
+
+instance Bounded (BoundedRatio PositiveUnitInterval Word64) where
+  minBound = BoundedRatio (1 % maxBound)
+  maxBound = BoundedRatio (1 % 1)
+
+instance Integral a => Bounded (BoundedRatio UnitInterval a) where
+  minBound = BoundedRatio (0 % 1)
+  maxBound = BoundedRatio (1 % 1)
 
 -- | Type to represent a value in the unit interval [0; 1]
-newtype UnitInterval = UnsafeUnitInterval (Ratio Word64)
+newtype UnitInterval = UnitInterval (Ratio Word64)
   deriving (Ord, Eq, Generic)
   deriving newtype (Show, NoThunks, NFData)
 
 instance Bounded UnitInterval where
-  minBound = UnsafeUnitInterval (0 % 1)
-  maxBound = UnsafeUnitInterval (1 % 1)
+  minBound = UnitInterval (0 % 1)
+  maxBound = UnitInterval (1 % 1)
 
 instance Default UnitInterval where
   def = minBound
 
 instance ToCBOR UnitInterval where
-  toCBOR (UnsafeUnitInterval u) = ratioToCBOR u
+  toCBOR (UnitInterval u) = ratioToCBOR u
 
 instance FromCBOR UnitInterval where
   fromCBOR = do
@@ -134,7 +233,7 @@ instance ToJSON UnitInterval where
   toJSON = toJSON . toScientificRoundToDecimalPlaces 19
 
 toScientificRoundToDecimalPlaces :: Int -> UnitInterval -> Scientific
-toScientificRoundToDecimalPlaces exp10 (UnsafeUnitInterval ui) =
+toScientificRoundToDecimalPlaces exp10 (UnitInterval ui) =
   scientific
     ((toInteger (numerator ui) * scale) `quot` toInteger (denominator ui))
     (- exp10)
@@ -157,12 +256,8 @@ fromScientificUnitInterval (normalize -> num) = do
     coeff = coefficient num
     exp10 = negate (base10Exponent num)
 
-
 unitIntervalToRational :: UnitInterval -> Rational
-unitIntervalToRational (UnsafeUnitInterval r) = promoteRatio r
-
-promoteRatio :: Integral a => Ratio a -> Rational
-promoteRatio r = toInteger (numerator r) % toInteger (denominator r)
+unitIntervalToRational (UnitInterval r) = promoteRatio r
 
 -- | Returns `Nothing` when supplied value is not in the [0, 1] range or when precision is
 -- too high.
@@ -180,7 +275,7 @@ unitIntervalFromRational :: Rational -> Maybe UnitInterval
 unitIntervalFromRational r
   | n < minVal || d < minVal || n > maxVal || d > maxVal = Nothing -- protect against overflow
   | r < 0 || r > 1 = Nothing -- ensure valid range
-  | otherwise = Just $ UnsafeUnitInterval $ fromInteger n % fromInteger d
+  | otherwise = Just $ UnitInterval $ fromInteger n % fromInteger d
   where
     minVal = toInteger (minBound :: Word64)
     maxVal = toInteger (maxBound :: Word64)
